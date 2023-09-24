f0a9b5ba754ea7f6014197120ea59b1b2cb084df
Haznto <haznto@gmail.com>
2023-09-24 08:21:43 +0000

    solved lab 39

--- src/components/Reducers/CategoryReducer.jsx
+++ src/components/Reducers/CategoryReducer.jsx
@@ -1,3 +1,4 @@
+import { createSlice } from "@reduxjs/toolkit"
 import axios from "axios"
 
 export let initialState = {
@@ -7,25 +8,43 @@ export let initialState = {
     toRender: []
 }
 
-
-export function categoryReducer(state = initialState, action) {
-    switch (action.type) {
-        case 'SET-ACTIVE-CATEGORY': {
-            return { ...state, activeCategory: action.payload }
+const categorySlice = createSlice({
+    name: 'categories',
+    initialState,
+    reducers: {
+        setActiveCategory(state, action) {
+            state.activeCategory = action.payload
+        },
+        setCategories(state, action) {
+            state.categories = action.payload
+        },
+        setProducts(state, action) {
+            state.products = action.payload
+        },
+        setRenderList(state, action) {
+            state.toRender = action.payload
         }
-        case 'SET-CURRENT-CATEGORY': {
-            return { ...state, categories: action.payload }
-        }
-        case 'SET-CURRENT-PRODUCT': {
-            return { ...state, products: action.payload }
-        }
-        case 'SET-RENDER-PRODUCTS': {
-            return { ...state, toRender: action.payload }
-        }
-        default: return state
     }
-}
+})
 
+// export function categoryReducer(state = initialState, action) {
+//     switch (action.type) {
+//         case 'SET-ACTIVE-CATEGORY': {
+//             return { ...state, activeCategory: action.payload }
+//         }
+//         case 'SET-CURRENT-CATEGORY': {
+//             return { ...state, categories: action.payload }
+//         }
+//         case 'SET-CURRENT-PRODUCT': {
+//             return { ...state, products: action.payload }
+//         }
+//         case 'SET-RENDER-PRODUCTS': {
+//             return { ...state, toRender: action.payload }
+//         }
+//         default: return state
+//     }
+// }
+export const { setActiveCategory, setCategories, setProducts, setRenderList } = categorySlice.actions
 export const getProducts = (activeCategory) => async dispatch => {
 
     try {
@@ -39,27 +58,30 @@ export const getProducts = (activeCategory) => async dispatch => {
     }
 }
 
-export const setActiveCategory = (payload) => {
-    return {
-        type: 'SET-ACTIVE-CATEGORY',
-        payload: payload
-    }
-}
-export const setCategories = (payload) => {
-    return {
-        type: 'SET-CURRENT-CATEGORY',
-        payload: payload
-    }
-}
-export const setProducts = (payload) => {
-    return {
-        type: 'SET-CURRENT-PRODUCT',
-        payload: payload
-    }
-}
-export const setRenderList = (payload) => {
-    return {
-        type: 'SET-RENDER-PRODUCTS',
-        payload: payload
-    }
-}
+// export const setActiveCategory = (payload) => {
+//     return {
+//         type: 'SET-ACTIVE-CATEGORY',
+//         payload: payload
+//     }
+// }
+// export const setCategories = (payload) => {
+//     return {
+//         type: 'SET-CURRENT-CATEGORY',
+//         payload: payload
+//     }
+// }
+// export const setProducts = (payload) => {
+//     return {
+//         type: 'SET-CURRENT-PRODUCT',
+//         payload: payload
+//     }
+// }
+// export const setRenderList = (payload) => {
+//     return {
+//         type: 'SET-RENDER-PRODUCTS',
+//         payload: payload
+//     }
+// }
+
+
+export default categorySlice.reducer
